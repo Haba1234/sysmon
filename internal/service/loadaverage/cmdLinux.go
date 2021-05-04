d4e5f19cca1238dcb01278da4618ce806148465a
Haba1234 <b_roman@inbox.ru>
2021-05-04 12:18:02 +0300

    проверка сборки под ios

--- internal/service/loadaverage/cmdLinux.go
+++ internal/service/loadaverage/cmdLinux.go
@@ -3,13 +3,13 @@ package loadaverage
 import (
 	"errors"
 	"fmt"
-	"io/ioutil"
+	"os/exec"
 	"runtime"
 )
 
 func runCMD() (string, error) {
 	if runtime.GOOS != "windows" {
-		/*grep := exec.Command("grep", "average")
+		grep := exec.Command("grep", "average")
 		top := exec.Command("top", "-bn1")
 		pipe, _ := top.StdoutPipe()
 		defer pipe.Close()
@@ -23,25 +23,17 @@ func runCMD() (string, error) {
 		fmt.Println(string(b))
 		return string(b), err
 
-		var info syscall.Sysinfo_t
-		err := syscall.Sysinfo(&info)
-		if err != nil {
-			return "", err
-		}
-		const shift = 16
-		str := fmt.Sprint(float64(info.Loads[0])/float64(1<<shift), " ")
-		str += fmt.Sprint(float64(info.Loads[1])/float64(1<<shift), " ")
-		str += fmt.Sprint(float64(info.Loads[2]) / float64(1<<shift))*/
+		/*
 
-		raw, err := ioutil.ReadFile("/proc/loadavg")
-		if err != nil {
-			return "", err
-		}
-		str := []string{"", "", ""}
-		fmt.Sscanf(string(raw), "%s %s %s",
-			&str[0], &str[1], &str[2])
+			raw, err := ioutil.ReadFile("/proc/loadavg")
+			if err != nil {
+				return "", err
+			}
+			str := []string{"", "", ""}
+			fmt.Sscanf(string(raw), "%s %s %s",
+				&str[0], &str[1], &str[2])
 
-		return str[0] + " " + str[1] + " " + str[1], nil
+			return str[0] + " " + str[1] + " " + str[1], nil*/
 	}
 	return "", errors.New("command 'load average' not supported operating system")
 }
