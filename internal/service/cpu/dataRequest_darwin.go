16b99efd50efdaabac68b689f0edb74cd5998f4a
Haba1234 <b_roman@inbox.ru>
2021-05-08 15:40:11 +0300

    тестирование сервисов для MAC

--- internal/service/cpu/dataRequest_darwin.go
+++ internal/service/cpu/dataRequest_darwin.go
@@ -21,14 +21,14 @@ func (cp *CPU) DataRequest() ([]float64, error) {
 	grep := exec.Command("grep", "Cpu(s)")
 	top := exec.Command("top", "-b", "-n1")
 
-	/*b, err := top.Output()
+	b, err := top.Output()
 	if err != nil {
 		log.Println("MAC OS.CPU v1 error:")
 		return nil, err
 	}
-	log.Println("MAC OS.CPU v1:", string(b))*/
+	log.Println("MAC OS.CPU v1:", string(b))
 
-	pipe, _ := top.StdoutPipe()
+	/*pipe, _ := top.StdoutPipe()
 	defer pipe.Close()
 	grep.Stdin = pipe
 	err = top.Start()
@@ -39,7 +39,7 @@ func (cp *CPU) DataRequest() ([]float64, error) {
 	if err != nil {
 		return nil, err
 	}
-	log.Println("MAC OS.CPU:", string(b))
+	log.Println("MAC OS.CPU:", string(b))*/
 	raw = strings.ReplaceAll(string(b), ", ", " ")
 	raw = strings.ReplaceAll(raw, "ni,", "ni ")
 	raw = strings.ReplaceAll(raw, ",", ".")
